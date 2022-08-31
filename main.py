7e6577f75afed75897e5ffb15b4f7d2874797b32
Joseph Wildey <joewildey@outlook.com>
2022-08-31 04:59:54 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import HPCurrency
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    HPCurrency.conversion()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
